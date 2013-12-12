38fbed0ec7b1dba7266985149ab1d126ce874128
Mark <m@rkw.me>
2013-12-12 08:49:19 +0000

    [OE-3856] soft deletion

--- migrations/m131205_130923_table_versioning.php
+++ migrations/m131205_130923_table_versioning.php
@@ -6,20 +6,20 @@ class m131205_130923_table_versioning extends CDbMigration
 	{
 		$this->execute("
 CREATE TABLE `et_ophcianaestheticpoassessment_anaesthesiasummary_version` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `event_id` int(10) unsigned NOT NULL,
-  `description` text COLLATE utf8_bin,
-  `last_modified_user_id` int(10) unsigned NOT NULL DEFAULT '1',
-  `last_modified_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
-  `created_user_id` int(10) unsigned NOT NULL DEFAULT '1',
-  `created_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
-  PRIMARY KEY (`id`),
-  KEY `acv_et_ophcianaestheticpoassessment_anaesthesiasummary_lmui_fk` (`last_modified_user_id`),
-  KEY `acv_et_ophcianaestheticpoassessment_anaesthesiasummary_cui_fk` (`created_user_id`),
-  KEY `acv_et_ophcianaestheticpoassessment_anaesthesiasummary_ev_fk` (`event_id`),
-  CONSTRAINT `acv_et_ophcianaestheticpoassessment_anaesthesiasummary_lmui_fk` FOREIGN KEY (`last_modified_user_id`) REFERENCES `user` (`id`),
-  CONSTRAINT `acv_et_ophcianaestheticpoassessment_anaesthesiasummary_cui_fk` FOREIGN KEY (`created_user_id`) REFERENCES `user` (`id`),
-  CONSTRAINT `acv_et_ophcianaestheticpoassessment_anaesthesiasummary_ev_fk` FOREIGN KEY (`event_id`) REFERENCES `event` (`id`)
+	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+	`event_id` int(10) unsigned NOT NULL,
+	`description` text COLLATE utf8_bin,
+	`last_modified_user_id` int(10) unsigned NOT NULL DEFAULT '1',
+	`last_modified_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
+	`created_user_id` int(10) unsigned NOT NULL DEFAULT '1',
+	`created_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
+	PRIMARY KEY (`id`),
+	KEY `acv_et_ophcianaestheticpoassessment_anaesthesiasummary_lmui_fk` (`last_modified_user_id`),
+	KEY `acv_et_ophcianaestheticpoassessment_anaesthesiasummary_cui_fk` (`created_user_id`),
+	KEY `acv_et_ophcianaestheticpoassessment_anaesthesiasummary_ev_fk` (`event_id`),
+	CONSTRAINT `acv_et_ophcianaestheticpoassessment_anaesthesiasummary_lmui_fk` FOREIGN KEY (`last_modified_user_id`) REFERENCES `user` (`id`),
+	CONSTRAINT `acv_et_ophcianaestheticpoassessment_anaesthesiasummary_cui_fk` FOREIGN KEY (`created_user_id`) REFERENCES `user` (`id`),
+	CONSTRAINT `acv_et_ophcianaestheticpoassessment_anaesthesiasummary_ev_fk` FOREIGN KEY (`event_id`) REFERENCES `event` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
 		");
 
@@ -37,25 +37,25 @@ CREATE TABLE `et_ophcianaestheticpoassessment_anaesthesiasummary_version` (
 
 		$this->execute("
 CREATE TABLE `et_ophcianaestheticpoassessment_dischargecriteria_version` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `event_id` int(10) unsigned NOT NULL,
-  `pain_score` tinyint(1) unsigned DEFAULT NULL,
-  `apvu` tinyint(1) unsigned DEFAULT NULL,
-  `mews_score` tinyint(1) unsigned DEFAULT NULL,
-  `nausea_vomiting` tinyint(1) unsigned DEFAULT NULL,
-  `blood_loss` tinyint(1) unsigned DEFAULT NULL,
-  `blood_sugar` tinyint(1) unsigned DEFAULT NULL,
-  `last_modified_user_id` int(10) unsigned NOT NULL DEFAULT '1',
-  `last_modified_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
-  `created_user_id` int(10) unsigned NOT NULL DEFAULT '1',
-  `created_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
-  PRIMARY KEY (`id`),
-  KEY `acv_et_ophcianaestheticpoassessment_dischargecriteria_lmui_fk` (`last_modified_user_id`),
-  KEY `acv_et_ophcianaestheticpoassessment_dischargecriteria_cui_fk` (`created_user_id`),
-  KEY `acv_et_ophcianaestheticpoassessment_dischargecriteria_ev_fk` (`event_id`),
-  CONSTRAINT `acv_et_ophcianaestheticpoassessment_dischargecriteria_lmui_fk` FOREIGN KEY (`last_modified_user_id`) REFERENCES `user` (`id`),
-  CONSTRAINT `acv_et_ophcianaestheticpoassessment_dischargecriteria_cui_fk` FOREIGN KEY (`created_user_id`) REFERENCES `user` (`id`),
-  CONSTRAINT `acv_et_ophcianaestheticpoassessment_dischargecriteria_ev_fk` FOREIGN KEY (`event_id`) REFERENCES `event` (`id`)
+	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+	`event_id` int(10) unsigned NOT NULL,
+	`pain_score` tinyint(1) unsigned DEFAULT NULL,
+	`apvu` tinyint(1) unsigned DEFAULT NULL,
+	`mews_score` tinyint(1) unsigned DEFAULT NULL,
+	`nausea_vomiting` tinyint(1) unsigned DEFAULT NULL,
+	`blood_loss` tinyint(1) unsigned DEFAULT NULL,
+	`blood_sugar` tinyint(1) unsigned DEFAULT NULL,
+	`last_modified_user_id` int(10) unsigned NOT NULL DEFAULT '1',
+	`last_modified_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
+	`created_user_id` int(10) unsigned NOT NULL DEFAULT '1',
+	`created_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
+	PRIMARY KEY (`id`),
+	KEY `acv_et_ophcianaestheticpoassessment_dischargecriteria_lmui_fk` (`last_modified_user_id`),
+	KEY `acv_et_ophcianaestheticpoassessment_dischargecriteria_cui_fk` (`created_user_id`),
+	KEY `acv_et_ophcianaestheticpoassessment_dischargecriteria_ev_fk` (`event_id`),
+	CONSTRAINT `acv_et_ophcianaestheticpoassessment_dischargecriteria_lmui_fk` FOREIGN KEY (`last_modified_user_id`) REFERENCES `user` (`id`),
+	CONSTRAINT `acv_et_ophcianaestheticpoassessment_dischargecriteria_cui_fk` FOREIGN KEY (`created_user_id`) REFERENCES `user` (`id`),
+	CONSTRAINT `acv_et_ophcianaestheticpoassessment_dischargecriteria_ev_fk` FOREIGN KEY (`event_id`) REFERENCES `event` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
 		");
 
@@ -70,10 +70,20 @@ CREATE TABLE `et_ophcianaestheticpoassessment_dischargecriteria_version` (
 		$this->addColumn('et_ophcianaestheticpoassessment_dischargecriteria_version','version_id','int(10) unsigned NOT NULL');
 		$this->addPrimaryKey('version_id','et_ophcianaestheticpoassessment_dischargecriteria_version','version_id');
 		$this->alterColumn('et_ophcianaestheticpoassessment_dischargecriteria_version','version_id','int(10) unsigned NOT NULL AUTO_INCREMENT');
+
+		$this->addColumn('et_ophcianaestheticpoassessment_anaesthesiasummary','deleted','tinyint(1) unsigned not null');
+		$this->addColumn('et_ophcianaestheticpoassessment_anaesthesiasummary_version','deleted','tinyint(1) unsigned not null');
+		$this->addColumn('et_ophcianaestheticpoassessment_dischargecriteria','deleted','tinyint(1) unsigned not null');
+		$this->addColumn('et_ophcianaestheticpoassessment_dischargecriteria_version','deleted','tinyint(1) unsigned not null');
 	}
 
 	public function down()
 	{
+		$this->dropColumn('et_ophcianaestheticpoassessment_anaesthesiasummary','deleted');
+		$this->dropColumn('et_ophcianaestheticpoassessment_anaesthesiasummary_version','deleted');
+		$this->dropColumn('et_ophcianaestheticpoassessment_dischargecriteria','deleted');
+		$this->dropColumn('et_ophcianaestheticpoassessment_dischargecriteria_version','deleted');
+
 		$this->dropTable('et_ophcianaestheticpoassessment_anaesthesiasummary_version');
 		$this->dropTable('et_ophcianaestheticpoassessment_dischargecriteria_version');
 	}
